791488837196908406075d2bcb88a8ca0041f0c1
ImMojoyoyo <towwwnsville@gmail.com>
2021-04-19 22:37:00 +0200

    v.0.0 with C

---
+++ birthday.c
@@ -0,0 +1,17 @@
+/*********** Programa para saber la edad según el año 2030 ***********/
+
+#include <stdio.h>
+#include <math.h>
+
+#define year 2030;
+
+int main (void)
+{
+    char name;
+    int born = 1995;
+    printf(" ¿Podrias decirme como te llamas? ");
+    scanf("%c\n", &name);
+
+    printf(" Este es tu nombre: %s", name);
+    
+}
